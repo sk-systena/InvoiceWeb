316e2dafba690d0a20afc178524bf3e3bd1eadfe
systena-suzukikenj <systena.suzukikenj@jppanasonic.onmicrosoft.com>
2018-02-23 19:29:14 +0900

    Webアプリ雛形登録

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ECT = require('ect');
 var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
@@ -13,13 +10,8 @@ var app = express();
 // view engine setup
 app.engine('ect', ectRenderer.render);
 app.set('view engine', 'ect');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
